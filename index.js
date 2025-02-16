f0cc0c65591750b4be38ed8a6d0d5a4cdc73e475
PamuSoul <johunyjohuuy@gmail.com>
2025-02-16 11:23:44 +0800

    HTML CSS JS 分離

---
+++ index.js
@@ -0,0 +1,22 @@
+function submitForm(action) {
+
+let form = document.getElementById("loginregister");
+let formData = new FormData(form);
+
+let url = (action === "register") 
+? "http://localhost:8080/api/v1/account/register" 
+: "http://localhost:8080/api/v1/account/login";
+
+
+fetch(url, {
+method: "POST",
+body: formData
+})
+.then(response => response.json())
+.then(data => {
+document.getElementById("responseMessage").innerText = data.message;
+})
+.catch(error => {
+console.error("錯誤:", error);
+});
+}
