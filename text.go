c621a79f3beca3cd254dffa1767f411878d67d00
PamuSoul <johunyjohuuy@gmail.com>
2025-02-17 14:56:16 +0800

    暫定修改

--- text.go
+++ text.go
@@ -1,46 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
-type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Username string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
-}
-
-var db *gorm.DB
+var db *sql.DB
 
 func main() {
 
 	dsn := "host=127.0.0.1 user=myuser password=mypassword dbname=username port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	var err error
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = sql.Open("postgres", dsn)
 	if err != nil {
-		log.Fatal("❌ 無法連接 postgres:", err)
+		log.Fatal("❌ 無法連接 PostgreSQL:", err)
 	}
-
-	db.AutoMigrate(&User{})
 
 	r := gin.Default()
 
 	r.POST("/api/v1/account/login", func(c *gin.Context) {
 		username := c.PostForm("username")
 		password := c.PostForm("password")
-		var user User
-		// 查詢資料庫中的使用者
-		if err := db.Where("username = ?", username).First(&user).Error; err != nil {
+
+		var storedPassword string
+		sqlStatement := "SELECT password FROM users WHERE username = $1"
+		err := db.QueryRow(sqlStatement, username).Scan(&storedPassword)
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"message": "帳號或密碼錯誤"})
 			return
 		}
-		if user.Password == password {
-			c.JSON(http.StatusOK, gin.H{"message": "登入成功", "username": username, "token": "aaaaa"})
+		if storedPassword == password {
+			c.JSON(http.StatusOK, gin.H{"message": "登入成功", "username": username, "token": "AAA"})
 		} else {
 			c.JSON(http.StatusUnauthorized, gin.H{"message": "帳號或密碼錯誤"})
 		}
